4e192d274af13f457661ed7be52c74e7deeb2858
Orkhan Guseinov <orxan4ik7777@gmail.com>
2023-10-06 18:07:25 +0400

    mealsScreen +

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals_apps/screens/categories_screen.dart';
+import 'package:meals_apps/data/dummy_data.dart';
+import 'package:meals_apps/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,7 +24,10 @@ class MealsApps extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: theme,
-      home: const CategoriesScreen(),
+      home: MealsScreen(
+        title: 'Someeee category...',
+        meals: dummyMeals,
+      ),
     );
   }
 }
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:meals_apps/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
 
+  final String title;
+  final List<Meal> meals;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Ой йо .. здесь ничего!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Попробуйте выбрать другуюю категорию !',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          )
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(meals[index].title),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
   }
 }
